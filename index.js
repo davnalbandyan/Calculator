9773c970d99a8a2c56124ec7dd78f3080a6d110e
davnalbandyan <davnalbandyan@gmail.com>
2023-10-09 08:48:24 +0400

    have finished the css part

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
-// const main = document.querySelector(".main");
-// const answer = document.querySelector(".answer");
-// const btns = document.querySelectorAll(".btns button");
-// const clear = document.querySelector(".clear");
+const main = document.querySelector(".main");
+const answer = document.querySelector(".answer");
+const btns = document.querySelectorAll(".btns button");
+const clear = document.querySelector(".clear");
 
-// // const calc = document.querySelectorAll(".btns .calc button");
+// const calc = document.querySelectorAll(".btns .calc button");
 
 
 
-// let arr = [] ;
+let arr = [] ;
 
-// btns.forEach(item => {
-//     item.addEventListener("click", () => {
-//         answer.value += item.textContent;
-//     });
-//     clear.addEventListener("click", () => {
-//         answer.value = "";
-//     });
+btns.forEach(item => {
+    item.addEventListener("click", () => {
+        answer.value += item.textContent;
+    });
+    clear.addEventListener("click", () => {
+        answer.value = "";
+    });
        
-// });
-
-
-const one = document.querySelector("#one")
-const two = document.querySelector("#two")
-const plus = document.querySelector(".plus")
-const equal = document.querySelector(".equal")
-
-const res = document.querySelector(".res")
-
-equal.addEventListener("click",()=>{
- let result = parseFloat(one.value) + parseFloat(two.value)
- console.log(result);
 });
+
+
 
 
 
